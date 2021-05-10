82533ab0505baf0f02f6f8beab52a62536772be8
lunghekani <lunghivlanga@gmail.com>
2021-05-10 04:52:59 +0200

    Add project files.

---
+++ 218051864-LV-LANGA-LAb Exercise-5/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Data_Layer;
+
+namespace _218051864_LV_LANGA_LAb_Exercise_5
+    /*LV Langa
+     * 218051864
+     * Student :)
+     * 
+     */
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            clsCSVOperations localCSVObject = new clsCSVOperations(); // created a local instance of the data layer class to access all of its methods
+            
+            fileDialogCSV.Title = "Please select a CSV file"; // making the window at the top prompt the user to select a CSV file
+            fileDialogCSV.Filter = "CSV files (*.csv)|*.csv"; // making sure the user can only enter a CSV file for processing
+
+            try// wrapped it in a try catch block to track any exceptions that get thrown
+            {
+
+            if (fileDialogCSV.ShowDialog() == DialogResult.OK) // if the user clicked okay
+            {
+                    dataGridView1.DataSource = localCSVObject.createCSVDataSource(fileDialogCSV.FileName); // setting the datasource for the gridview
+            }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message); // throwing the exception message to the user
+                
+            }
+        }s
+
+        private void button2_Click(object sender, EventArgs e)
+        {// code block used to exit the application
+            MessageBox.Show("Goodbye and go live a transformed life :)");
+            Application.Exit();
+        }
+    }
+}
---
+++ Data Layer/Class1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Data;
+
+namespace Data_Layer
+{
+    public class Class1
+    {
+    }
+    public class clsCSVOperations {
+
+        public DataTable createCSVDataSource(string filepath) {
+            DataTable dtCSV = new DataTable();
+            StreamReader csvStream = new StreamReader(filepath); // using a stream reader to read the CSV line by line
+            List<string> dataSourceColumns = new List<string>(); // Tracking the columns into a list
+            List<string[]> dataSourceRows = new List<string[]>(); // tracking the rows into a list
+            
+            string templine = csvStream.ReadLine(); // used to read the first line of the CSV because that is almost always the line with the columns
+
+            
+            var tempCols = templine.Split(',');// splitting by the ','
+            
+            
+            foreach (var item in tempCols) // loop used to create each column
+            {
+                dtCSV.Columns.Add(item);
+            }
+            
+
+            while (!csvStream.EndOfStream)
+            {
+                string line = csvStream.ReadLine(); // reading line by line 
+
+                dataSourceRows.Add(Regex.Split(line, ",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)")); // splitting using regex incase the CSV has column values that include a , and break the damn CSV
+
+            }
+            foreach (var item in dataSourceRows) // loop used to add rows to the data table
+            {
+                dtCSV.Rows.Add(item);
+            }
+            return dtCSV; // returning the datatable 
+
+        }
+
+    }
+}
